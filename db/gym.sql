5951812573b1749920a847da763d1556dc448ac8
Carlos Zubillaga <carloszubillaga@Carloss-MacBook-Pro.local>
2021-09-24 14:55:22 +0100

    first commit

---
+++ db/gym.sql
@@ -0,0 +1,25 @@
+DROP TABLE bookings;
+DROP TABLE members;
+DROP TABLE lessons;
+
+CREATE TABLE lessons (
+    id SERIAL PRIMARY KEY,
+    date VARCHAR(255),
+    time VARCHAR(255),
+    description VARCHAR (255),
+    duration INT,
+    capacity INT
+);
+
+CREATE TABLE members (
+    id SERIAL PRIMARY KEY,
+    first VARCHAR,
+    last VARCHAR, 
+    type VARCHAR
+);
+
+CREATE TABLE bookings (
+    id SERIAL PRIMARY KEY,
+    lesson_id INT REFERENCES lessons(id) ON DELETE CASCADE,
+    member_id INT REFERENCES members(id) ON DELETE CASCADE
+);
---
+++ models/member.py
@@ -0,0 +1,6 @@
+class Member: 
+
+    def __init__(self, name, type, id):
+        self.name = name 
+        self.type = type
+        self.id = id
