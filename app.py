4d6c9f205e2b1af88b9ad193a5462395b8647369
Carlos Zubillaga <carloszubillaga@Carloss-MacBook-Pro.local>
2021-09-25 08:50:03 +0100

    Added lessons and lessons/show routes

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
+from controllers.lesson_controller import lessons_blueprint
 
 app = Flask(__name__)
+
+app.register_blueprint(lessons_blueprint)
+
+@app.route('/')
+def home():
+    return render_template('index.html', title='Home')
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ controllers/lesson_controller.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request, redirect
+from flask import Blueprint
+from models.lesson import Lesson
+import repositories.lesson_repository as lesson_repository
+
+lessons_blueprint = Blueprint('lessons', __name__)
+
+@lessons_blueprint.route('/upcoming')
+def upcoming():
+    lessons = lesson_repository.select_all()
+    return render_template('lessons/index.html', title='Upcoming', lessons=lessons)
+
+@lessons_blueprint.route('/show/<id>')
+def show(id):
+    lesson = lesson_repository.select(id)
+    return render_template('lessons/show.html', title=lesson.description, lesson=lesson)
