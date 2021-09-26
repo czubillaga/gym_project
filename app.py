944136e9304450e8f78ba12d1015c8049eb98fa7
Carlos Zubillaga <carloszubillaga@Carloss-MacBook-Pro.local>
2021-09-26 15:40:28 +0100

    Added bookings and bookings/show routes

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
 from controllers.lesson_controller import lessons_blueprint
 from controllers.member_controller import members_blueprint
+from controllers.booking_controller import bookings_blueprint
 
 app = Flask(__name__)
 
 app.register_blueprint(lessons_blueprint)
 app.register_blueprint(members_blueprint)
+app.register_blueprint(bookings_blueprint)
 
 @app.route('/')
 def home():
